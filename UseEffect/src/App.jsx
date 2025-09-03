0c92d521af91ff1d4a143375274cbb0e146cf7ad
AdnanBarud <barudyt@gmail.com>
2025-09-03 21:52:05 +0530

    useEffect variotion 4 run when count or total changes in dono me se koi bhi change hua to useEffect run karenga

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [total, setTotal] = useState(1)
   // variontion 1
   // useEffect(() => {
   //  alert(`i will run on every render`)
@@ -15,18 +15,29 @@ function App() {
   // }, [])
   
   // variontion 3
+  // useEffect(() => {
+  //   alert(`i will run when count changes`)
+  // }, [count])
+
+  // variotion 4
   useEffect(() => {
-    alert(`i will run when count changes`)
-  }, [count])
+    alert(`i will run when count or total changes`)
+  }, [count, total])
   
+
   function handleClick() {
     setCount(count + 1)
+  }
+   function handleTotal() {
+    setTotal(total + 1)
   }
 
   return (
     <div>
       <button onClick={handleClick}>Click me</button>
       <p>Count is: {count}</p>
+       <button onClick={handleTotal}>Click me</button>
+      <p>Count is: {total}</p>
     </div>
   )
 }
