49ddea3472a356cc4f2cea1ef542ba9ace8e4ad4
AdnanBarud <barudyt@gmail.com>
2025-09-12 22:31:51 +0530

    created basic setup

--- ReactRouting/basicReact/src/App.jsx
+++ ReactRouting/basicReact/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {  RouteConfig,
+  route,
+} from "@react-router/dev/routes";
 
-function App() {
-  const [count, setCount] = useState(0)
+ const router=createBrowserRouter(
+  [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+  ]
+ )
+
+function App() {const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+
     </div>
   )
 }
