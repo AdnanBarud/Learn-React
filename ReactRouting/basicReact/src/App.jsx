7fe22b0668b2cc06feade9989037f1585b2648bd
AdnanBarud <barudyt@gmail.com>
2025-09-12 22:38:24 +0530

    created basic routing application

--- ReactRouting/basicReact/src/App.jsx
+++ ReactRouting/basicReact/src/App.jsx
@@ -1,27 +1,32 @@
 import './App.css'
-import {  RouteConfig,
-  route,
-} from "@react-router/dev/routes";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
 
  const router=createBrowserRouter(
   [
     {
-
+      path:'/',
+      element: <Home/>
     },
     {
-
+      path:'/about',
+      element: <About/>
     },
     {
-
+      path:'/contact',
+      element: <Contact />
     }
   ]
  )
 
-function App() {const [count, setCount] = useState(0)
+function App(){
 
   return (
     <div>
-
+      <RouterProvider router={router} />
     </div>
   )
 }
