4de8cb56c0be3ece3006b0560b11c7b6993152f5
AdnanBarud <barudyt@gmail.com>
2025-09-20 00:34:38 +0530

    created start watch with use of setIntervel

--- UseRef/useRef/src/App.jsx
+++ UseRef/useRef/src/App.jsx
@@ -2,24 +2,37 @@ import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const var1 = useRef(0);
-  const btnRef = useRef();
+  const [time, setTime] = useState(0)
+  const timerRef = useRef();
 
-  function handlRef(){
-    var1.current +=1;
-    console.log(var1.current);
-    
+  // const var1 = useRef(0);
+  // const btnRef = useRef();
+  // function handlRef(){
+  //   var1.current +=1;
+  //   console.log(var1.current);
+  // }
+  // function changeColor(){
+  //   btnRef.current.style.backgroundColor = 'red';
+  // }
+  function handleStart(){
+    setInterval(() => {
+      setTime(time => time + 1)
+    }, 1000);
   }
-
-  function changeColor(){
-    btnRef.current.style.backgroundColor = 'red';
-  }
+  function handelStop(){}
+  function handelReset(){}
+  
   return (
     <div>
-      <button ref={btnRef} onClick={handlRef}>a</button>
+      <h1>Time in Seconts {time}</h1>
+      <button onClick={handleStart}> Start</button>
+      <br></br>
+      <button onClick={handelStop}> Start</button>
+      <br></br>
+      <button onClick={handelReset}> Start</button>
+      {/* <button ref={btnRef} onClick={handlRef}>a</button>
       <br />
-      <button onClick={changeColor}>color change</button>
+      <button onClick={changeColor}>color change</button> */}
 
     </div>
   )
