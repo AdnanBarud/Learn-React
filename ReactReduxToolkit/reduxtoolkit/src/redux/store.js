2d3b129b342bb9efb0bb1061295e0275b4a550a7
AdnanBarud <barudyt@gmail.com>
2025-09-19 23:16:38 +0530

    added slice to store

--- ReactReduxToolkit/reduxtoolkit/src/redux/store.js
+++ ReactReduxToolkit/reduxtoolkit/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/count/countSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 })
