2aed8e228c6e499e66d2d1aaa23bcdd34cf6a4ba
AdnanBarud <barudyt@gmail.com>
2025-09-20 00:12:50 +0530

    useRef first useCase for creating variable who is not render

--- UseRef/useRef/src/App.jsx
+++ UseRef/useRef/src/App.jsx
@@ -3,10 +3,17 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const btnRef = useRef(0);
 
+  function handlRef(){
+    btnRef.current +=1;
+    console.log(btnRef.current);
+    
+  }
   return (
     <div>
-      
+      <button onClick={handlRef}>a</button>
+
     </div>
   )
 }
