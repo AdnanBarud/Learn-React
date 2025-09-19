2ba97f606e1e9a9563aada325413c503d50cba1a
AdnanBarud <barudyt@gmail.com>
2025-09-20 00:19:03 +0530

    useRef secound use case for creating variable who can refrer to any btn or component for using or changing ther dom

--- UseRef/useRef/src/App.jsx
+++ UseRef/useRef/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const btnRef = useRef(0);
+  const var1 = useRef(0);
+  const btnRef = useRef();
 
   function handlRef(){
-    btnRef.current +=1;
-    console.log(btnRef.current);
+    var1.current +=1;
+    console.log(var1.current);
     
+  }
+
+  function changeColor(){
+    btnRef.current.style.backgroundColor = 'red';
   }
   return (
     <div>
-      <button onClick={handlRef}>a</button>
+      <button ref={btnRef} onClick={handlRef}>a</button>
+      <br />
+      <button onClick={changeColor}>color change</button>
 
     </div>
   )
