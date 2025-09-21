69a4bc547877e7c9449cbbcb074ad0fdb98de195
AdnanBarud <barudyt@gmail.com>
2025-09-21 23:54:54 +0530

    before useMemo is very slow

--- useMemo/useMemo/src/App.jsx
+++ useMemo/useMemo/src/App.jsx
@@ -3,11 +3,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+
+  function complexCalculate(num) {
+    console.log('Calculating...')
+    for(let i=0; i<=1000000000; i++) {}
+    return num*2
+  }
+
+  const calculation = complexCalculate(input)
 
   return (
    <div>
     <h1>Count: {count}</h1>
     <button onClick={() => setCount(count+1)}>increment</button>
+    <br />
+    <input type="number" value={input} onChange={(e) => setInput(e.target.value)} />
+    <br />
+    number: {calculation}
    </div>
   )
 }
