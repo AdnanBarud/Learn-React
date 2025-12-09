dc3e93b804dfff46cb14b11ff8c3749e5cde99d2
AdnanBarud <barudyt@gmail.com>
2025-12-09 12:10:10 +0530

    starting with basic and i learn about jsx component and state useState Hooks

---
+++ Second React/Basic react/vite-project/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = (props) => {
+  return (
+    <div>
+        <h1>Navbar Component</h1>
+        <p>count value from app.jsx is {props.count}</p>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ Second React/Basic react/vite-project/src/Components/Props.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Props = () => {
+    
+  return (
+    <div>
+        <h1>Props Component</h1>
+    </div>
+  )
+}
+
+export default Props
